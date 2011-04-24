5b0f80cf986037dce4614a7342bb628469f57f80
O. Can Bascil <ocanbascil@gmail.com>
2011-04-24 21:28:35 +0100

    New tests for time util

--- tests/test/test_timeutil.py
+++ tests/test/test_timeutil.py
@@ -49,13 +49,38 @@ class GetTest(unittest.TestCase):
   def test_hour_expiration(self):
     default_exp =  time_util.hour_expiration(_test_datetime=self.time)
     value_exp = time_util.hour_expiration(5,_test_datetime=self.time)
+    minute_offset_exp = time_util.hour_expiration(minute_offset=30,_test_datetime=self.time)
     hour_offset_exp = time_util.hour_expiration(hour_offset=2,_test_datetime=self.time)
-    hour_value_hour_offset_exp = time_util.hour_expiration(5,hour_offset=2,_test_datetime=self.time)
+    hour_minute_offset_exp = time_util.hour_expiration(hour_offset=2,minute_offset=30,_test_datetime=self.time)
+    value_hour_offset_exp = time_util.hour_expiration(5,hour_offset=2,_test_datetime=self.time)
+    value_hour_minute_offset_exp = time_util.hour_expiration(5,hour_offset=2,minute_offset=30,_test_datetime=self.time)
     
     self.assertEqual(default_exp,3600)
     self.assertEqual(value_exp,18000)
-    self.assertEqual(hour_offset_exp,10800) 
-    self.assertEqual(hour_value_hour_offset_exp,25200)    
+    self.assertEqual(minute_offset_exp,5400)
+    self.assertEqual(hour_offset_exp,10800) #Problem here 
+    self.assertEqual(hour_minute_offset_exp,12600) 
+    self.assertEqual(value_hour_offset_exp,25200)    
+    self.assertEqual(value_hour_minute_offset_exp,27000)     
+    
+    self.time = datetime(2011,1,1,3,30,0)
+
+    default_exp =  time_util.hour_expiration(_test_datetime=self.time)
+    value_exp = time_util.hour_expiration(5,_test_datetime=self.time)
+    minute_offset_exp = time_util.hour_expiration(minute_offset=30,_test_datetime=self.time)
+    hour_offset_exp = time_util.hour_expiration(hour_offset=2,_test_datetime=self.time)
+    hour_minute_offset_exp = time_util.hour_expiration(hour_offset=2,minute_offset=30,_test_datetime=self.time)
+    value_hour_offset_exp = time_util.hour_expiration(5,hour_offset=2,_test_datetime=self.time)
+    value_hour_minute_offset_exp = time_util.hour_expiration(5,hour_offset=2,minute_offset=30,_test_datetime=self.time)
+    
+    print 'default_exp %s' %default_exp
+    print 'value_exp %s' %value_exp
+    print 'minute_offset_exp %s' %minute_offset_exp
+    print 'hour_offset_exp %s' %hour_offset_exp
+    print 'hour_minute_offset_exp %s' %hour_minute_offset_exp
+    print 'value_hour_offset_exp %s' %value_hour_offset_exp
+    print 'value_hour_minute_offset_exp %s' %value_hour_minute_offset_exp
+    
     
   def test_day_expiration(self):
     print time_util.day_expiration(1,_test_datetime=self.time)
